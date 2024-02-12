7f2e2cf30ad33c25a2018dfa177942bdf3ff7b5e
Darien Arroyo <darien.arroyo.castro@est.una.ac.cr>
2024-02-12 14:05:57 -0600

    Added the logic of methods in PlayerCard.cpp

--- main/PlayerCard.cpp
+++ main/PlayerCard.cpp
@@ -1,1 +1,65 @@
 #include "PlayerCard.h"
+
+
+PlayerCard::PlayerCard() {}
+
+PlayerCard::PlayerCard(int money) {
+    this->money = money;
+    this->playerCards = new string[numCards];
+}
+
+void PlayerCard::getCards(Deck deck) {
+    for (int i = 0; i < numCards; i++) {
+        playerCards[i] = deck.getCard();
+    }
+}
+
+int PlayerCard::getNumCards()
+{
+    return numCards;
+}
+
+int PlayerCard::getMoney()
+{
+    return this->money;
+}
+
+string* PlayerCard::getPlayerCards()
+{
+    return playerCards;
+}
+
+Texture PlayerCard::getCardTexture(int index)
+{
+    return cardsTextures[index];
+}
+
+void PlayerCard::setCardTextures(Texture cardTexture, int index)
+{
+    cardsTextures[index] = cardTexture;
+}
+
+void PlayerCard::carryBet(int newBet, int& actualBet) {
+    actualBet += newBet;
+    this->money = money - newBet;
+}
+
+Texture PlayerCard::drawPlayer(int numPlayer, Texture texture)
+{
+    string player = to_string(numPlayer);
+    texture.loadFromFile("player" + player + ".png");
+    return texture;
+}
+
+void PlayerCard::printArray()
+{
+    for (int i = 0; i < numCards; i++) {
+        cout << playerCards[i] << " ";
+    }
+}
+
+void PlayerCard::deleteArray()
+{
+    delete[] playerCards;
+}
+
