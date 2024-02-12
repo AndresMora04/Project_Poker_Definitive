6fcdb2ab3ac76c3ab8d1d7ebe38197bf5051b13a
Andres Mora <jose.mora.mora@est.una.ac.cr>
2024-02-12 10:09:03 -0600

    Added information in Deck.h

--- main/Deck.h
+++ main/Deck.h
@@ -1,5 +1,21 @@
 #pragma once
+#pragma once
+#include "define.h"
 class Deck
 {
+private:
+    string suits[NUM_SUITS] = { "Corazones", "Diamantes", "Treboles", "Picas" };
+    string ranks[NUM_RANKS] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    string cards[NUM_SUITS][NUM_RANKS];
+    Texture cardTextures[NUM_SUITS][NUM_RANKS];
+public:
+    Deck();
+    void DeckTexture();
+    string* getFlop();
+    string getTurn();
+    string getRiver();
+    void print();
+    Texture printTextures(string, string, Texture);
+    void deleteArray(string*);
+    string getCard();
 };
-
