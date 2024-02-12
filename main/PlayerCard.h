02058f739f39edfa7fa180fc3bed44d33cca32cc
Darien Arroyo <darien.arroyo.castro@est.una.ac.cr>
2024-02-12 13:47:45 -0600

    Added information in PlayerCard.h

--- main/PlayerCard.h
+++ main/PlayerCard.h
@@ -1,5 +1,24 @@
 #pragma once
-class PlayerCard
-{
+#include "define.h"
+#include "Deck.h"
+class PlayerCard {
+private:
+    Deck deck;
+    string* playerCards;
+    Texture cardsTextures[4];
+    int numCards = 4;
+    int money = 0;
+public:
+    PlayerCard();
+    PlayerCard(int);
+    void getCards(Deck);
+    int getNumCards();
+    int getMoney();
+    string* getPlayerCards();
+    Texture getCardTexture(int);
+    void setCardTextures(Texture, int);
+    void carryBet(int, int&);
+    Texture drawPlayer(int, Texture);
+    void printArray();
+    void deleteArray();
 };
-
