e2e05c1fbceced208306c3e633dc09e025f90fdc
Andres Mora <jose.mora.mora@est.una.ac.cr>
2024-02-12 13:53:38 -0600

    Added methods in Dealer.h

--- main/Dealer.h
+++ main/Dealer.h
@@ -1,5 +1,23 @@
 #pragma once
-class Dealer
-{
+#include "Deck.h"
+#include "PlayerCard.h"
+class Dealer {
+private:
+    string* showedCards;
+    Deck deck;
+    Texture cardTextures[4];
+    int cardsSize = 0;
+public:
+    Dealer();
+    Dealer(Deck);
+    string* getPlayerCards(PlayerCard playerCard);
+    string* getFlop();
+    string getTurn();
+    string getRiver();
+    void setCardTextures(string, int);
+    string* getShowedCards();
+    Texture getTexture(int);
+    void fillShowedCards(string, int&);
+    int getCardsSize();
+    void deleteArray();
 };
-
