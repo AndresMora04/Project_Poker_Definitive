091decda4444dcaedc0b97046016a4c028316b58
Andres Mora <jose.mora.mora@est.una.ac.cr>
2024-02-12 05:20:46 -0600

    Added SFML library

--- main/define.h
+++ main/define.h
@@ -1,6 +1,12 @@
 #pragma once
-class define
-{
-
-};
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <SFML/Graphics.hpp> 
+#include <stdlib.h>
+#include <time.h>
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+using namespace std;
+using namespace sf;
 
